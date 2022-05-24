caf166da14d1671323cc26f333dca4608ee4609e
rrvallar <rrvallar@gmail.com>
2022-05-24 14:56:51 -0400

    added arrays

--- develop/script.js
+++ develop/script.js
@@ -1,25 +1,84 @@
 // Assignment code here
 
 // Input variables
-var enter;
-var acceptNumber;
-var acceptUppercase;
-var acceptLowercase;
-var acceptCharacter;
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+let enter;
+let acceptLength;
+let acceptNumber;
+let acceptUppercase;
+let acceptLowercase;
+let acceptCharacter;
 
-function generatePassword() {
-  console.log("button-works");
-  // password length/value
+// Arrays for special characters, upper&lowercase and numbers
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+let upperCase = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+let lowerCase = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+let numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+let specialCharacters = [
+  "!",
+  "%",
+  "&",
+  ",",
+  "*",
+  "+",
+  "-",
+  ".",
+  "/",
+  "<",
+  ">",
+  "?",
+  "~",
+];
